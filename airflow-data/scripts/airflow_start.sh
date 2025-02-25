7c05e386b7d9fce8a6263568cc9f530cfa375ac5
felipene0 <felipeepilefcv12@gmail.com>
2025-02-25 11:54:01 -0300

    fixing docker-compose airflow werbserver error

--- airflow-data/scripts/airflow_start.sh
+++ airflow-data/scripts/airflow_start.sh
@@ -6,6 +6,17 @@ if [ -e "/opt/airflow/requirements.txt" ]; then
   $(command -v pip) install --no-cache-dir -r /opt/airflow/requirements.txt
 fi
 
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init && \
+  airflow users create \
+    --username airflow \
+    --firstname airflow \
+    --lastname airflow \
+    --role Admin \
+    --email airflow@airflow.com \
+    --password airflow
+fi
+
 $(command -v airflow) db upgrade
 
 exec airflow webserver
